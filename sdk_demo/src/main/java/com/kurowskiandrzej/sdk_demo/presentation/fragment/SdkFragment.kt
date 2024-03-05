276c0b2dfe9824cb63c4a1d8aa3eb923ef42a866
Andrzej Kurowski <kurowskiandrzej@mail.com>
2024-03-05 14:09:54 +0100

    update SdkFragment

--- sdk_demo/src/main/java/com/kurowskiandrzej/sdk_demo/presentation/fragment/SdkFragment.kt
+++ sdk_demo/src/main/java/com/kurowskiandrzej/sdk_demo/presentation/fragment/SdkFragment.kt
@@ -1,24 +1,26 @@
 package com.kurowskiandrzej.sdk_demo.presentation.fragment
 
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.compose.ui.platform.ComposeView
-import androidx.fragment.app.Fragment
+import com.kurowskiandrzej.sdk_demo.R
 import com.kurowskiandrzej.sdk_demo.presentation.nav.SdkApp
 
 class SdkFragment : Fragment() {
 
     override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
+        inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View {
-        return ComposeView(requireContext()).apply {
-            setContent {
-                SdkApp()
-            }
+    ): View? {
+        val cvSdk = view?.findViewById<ComposeView>(R.id.cvSdk)
+
+        cvSdk?.setContent {
+            SdkApp()
         }
+
+        return inflater.inflate(R.layout.fragment_sdk, container, false)
     }
 }
